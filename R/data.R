d910e61d10f4c172b3611e7d75ce5d25ece37d79
Vince Carey <stvjc@channing.harvard.edu>
2019-07-13 15:44:03 -0400

    updated with igf_giant150

--- R/data.R
+++ R/data.R
@@ -4,3 +4,8 @@
 #' @format GenomicFiles instance
 #' @source \url{https://portals.broadinstitute.org/collaboration/giant/index.php/GIANT_consortium_data_files}
 "WHRadjBMI_gf"
+
+#' an indexedGF instance for 150 files in GIANT consortium portal
+#' @docType data
+#' @format indexedGF instance
+"igf_giant150"
