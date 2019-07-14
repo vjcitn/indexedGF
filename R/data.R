ad68fbd9570d90ce368bf330b5feff39f02a5566
Vince Carey <stvjc@channing.harvard.edu>
2019-07-13 23:04:48 -0400

    updating

--- R/data.R
+++ R/data.R
@@ -9,3 +9,26 @@
 #' @docType data
 #' @format indexedGF instance
 "igf_giant150"
+
+#' an indexedGF instance for 149 files in GIANT consortium portal
+#' @docType data
+#' @format indexedGF instance
+#' @note A small 'enrichment' dataset was dropped from the full
+#' set of 150 files.  Metadata components include 'field_names'
+#' and 'nsnps' which are coordinated with the 149 files but
+#' have no element names.  The filenames used here assume the
+#' data are locally available in the current folder.  See aws_igf_GIANT149
+#' for access to globally accessible images in AWS S3.
+"igf_GIANT149"
+
+#' an indexedGF instance for 149 files in GIANT consortium portal, resident in AWS S3
+#' @docType data
+#' @format indexedGF instance
+#' @note A small 'enrichment' dataset was dropped from the full
+#' set of 150 files.  Metadata components include 'field_names'
+#' and 'nsnps' which are coordinated with the 149 files but
+#' have no element names.  Note that there has been light curation
+#' relative to material at the GIANT portal, where some of the files
+#' have Pax headers indicative of compression in tar archive
+#' context, and some files are not compressed despite their suffix.
+"aws_igf_GIANT149"
